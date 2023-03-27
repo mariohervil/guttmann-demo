13b0416ab15625c3ed5eb8d2b7e34e9bfec8eb62
mariohervil <mariohervil@campus.monlau.com>
2023-03-27 17:27:02 +0200

    Initial commit

--- server/src/config.ts
+++ server/src/config.ts
@@ -2,5 +2,4 @@ const Config = {
 	port: process.env.PORT || 8080,
 	connectionString: `mongodb+srv://root:${process.env.MONGOPWD}@guttmann.nlwqyzd.mongodb.net/?retryWrites=true&w=majority`,
 };
-//I4WY44DoTD0t6qBw
 export default Config;
